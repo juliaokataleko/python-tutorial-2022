4811fb3f091872698f0c928a77f25df45b6e351b
juliaokataleko <juliofeli78@gmail.com>
2022-04-04 11:50:52 +0000

    if statement

---
+++ 10-if.py
@@ -0,0 +1,13 @@
+temperature = 25
+
+if temperature > 30:
+    print("It's a hot day")
+    print("Dirnk plenty of water")
+elif temperature > 20: # (20, 30)
+    print("It's a nice day")
+elif temperature > 10:
+    print("It's a bit cold")
+else:
+    print("It's cold")
+    
+print("Done!")
