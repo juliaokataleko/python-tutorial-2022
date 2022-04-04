83f950c297c799cb5a3caa4219ce4b2a4207d8da
juliaokataleko <juliofeli78@gmail.com>
2022-04-04 12:22:07 +0000

    lists

---
+++ 13-lists.py
@@ -0,0 +1,6 @@
+names = ["John", "Bob", "Mosh", "Sam","Mary"]
+
+names[0] = "Jon"
+
+print(names[0:3])
+print(names)
---
+++ 14-lists_methods.py
@@ -0,0 +1,15 @@
+numbers = [1,2,3,4,5,6,7,8,9]
+
+numbers.append(10)
+print(numbers)
+
+numbers.insert(0, -5)
+print(numbers)
+
+numbers.remove(3)
+print(numbers)
+
+# numbers.clear()
+print(numbers)
+
+print(1 in numbers)
