79c186adb1d9ccb38c91a2958a06cc1f5ba72d99
juliaokataleko <juliofeli78@gmail.com>
2022-04-04 11:28:10 +0000

    logical operators

---
+++ 09-logical_operators.py
@@ -0,0 +1,12 @@
+price = 100
+
+print(price > 10 and price < 20)
+print(price > 10 or price < 20)
+
+# not operator
+print(not price < 10)
+
+# logical operators description
+# and -> all conditions
+# or -> (at least one)
+# not -> negation
